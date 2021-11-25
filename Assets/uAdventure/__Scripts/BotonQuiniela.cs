2adc7b2392b00cd15f9b562e5c6b7514f252dff8
Andrés de la Cuesta López <48914050+ancues02@users.noreply.github.com>
2021-11-25 15:45:05 +0100

    metida la escena de pablo

---
+++ Assets/uAdventure/__Scripts/BotonQuiniela.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonQuiniela : MonoBehaviour
+{
+    Button button;
+    public float valueMod;
+    public BetManager rm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    
+    public void OnClick()
+    {
+        ColorBlock aux = button.colors;
+        //rm.ModifyValue(-valueMod);
+        aux.normalColor = Color.yellow;
+        aux.highlightedColor = Color.yellow;
+        aux.selectedColor = Color.yellow;
+        aux.pressedColor = Color.yellow;
+        button.colors = aux;
+
+
+    }
+
+}
